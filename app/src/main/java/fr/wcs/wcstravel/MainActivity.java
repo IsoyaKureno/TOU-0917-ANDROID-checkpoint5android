b86f32783e682816708fa0598ea7f3b7f1519ed4
Isoya Kureno <yerle.nicolas@gmail.com>
2018-01-24 11:07:36 +0100

    init2

--- app/src/main/java/fr/wcs/wcstravel/MainActivity.java
+++ app/src/main/java/fr/wcs/wcstravel/MainActivity.java
@@ -20,9 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "Value is: ");
-
-
 
         mMyStudentsRef.addValueEventListener(new ValueEventListener() {
             @Override public void onDataChange(DataSnapshot dataSnapshot) {
